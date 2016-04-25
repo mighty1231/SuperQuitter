69cad0326d9585c6929584ed78e5fe0ea5bd5509
S.H.Lee <mighty1231@gmail.com>
2016-04-25 22:24:04 +0900

    version 1.0.0.2 release

--- sqlog.c
+++ sqlog.c
@@ -32,8 +32,27 @@ int sqlog(wchar_t *format, ...){
 	wcscpy(pwc+1, L"SuperQuitter_log.txt");
 
 	FILE *pFile;
+	size_t file_size; WORD first;
 	{
-		pFile = _wfopen(logfname, L"a");
+		pFile = _wfopen(logfname, L"r+");
+		if (pFile != 0) {
+			fseek(pFile, 0, SEEK_SET);
+			fread(&first, sizeof(WORD), 1, pFile);
+			fseek(pFile, 0, SEEK_END);
+			file_size = ftell(pFile);
+			fclose(pFile);
+		} else {
+			file_size = 0;
+		}
+	}
+	{
+		if (first != 0xFEFF || file_size <= 2) {
+			pFile = _wfopen(logfname, L"wb");
+			WORD mark = 0xFEFF;
+			fwrite(&mark, sizeof(WORD), 1, pFile);
+		} else {
+			pFile = _wfopen(logfname, L"ab");
+		}
 
 		/* Time */
 		fwprintf(pFile, L"[%4d.%02d.%02d %02d:%02d:%02d] ",
@@ -47,7 +66,7 @@ int sqlog(wchar_t *format, ...){
 
 		/* End of line */
 		fwprintf(pFile, L"\r\n");
+		fclose(pFile);
 	}
-	fclose(pFile);
 	return 0;
 }
