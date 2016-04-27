ce8419a48c5b78e521522dfbefb190758443b010
S.H.Lee <mighty1231@gmail.com>
2016-04-27 16:54:56 +0900

    Recognize starcraft just comparing code to patch. Release 1.0.0.4

--- resources.h
+++ resources.h
@@ -1,4 +1,4 @@
 #define IDC_QUIT2MNUBIN 122
-#define _VERSION "1.0.0.3"
-#define _VERSION_RCS "1.0.0.3\0"
-#define _VERSION_RCT 1,0,0,3
+#define _VERSION "1.0.0.4"
+#define _VERSION_RCS "1.0.0.4\0"
+#define _VERSION_RCT 1,0,0,4
