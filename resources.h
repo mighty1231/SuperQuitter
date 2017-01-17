6ccd0df1653c5e653215e602afecf07277e936be
SeongHoi <mighty1231@gmail.com>
2017-01-17 10:44:45 +0900

    Release 1.0.0.7, do not set hook at first

--- resources.h
+++ resources.h
@@ -1,6 +1,6 @@
 #define IDC_QUIT2MNUBIN 122
 #define _FILENAME "SuperQuitter.bwl"
 #define _FILENAME_UNI L"SuperQuitter.bwl"
-#define _VERSION "1.0.0.6"
-#define _VERSION_RCS "1.0.0.6\0"
-#define _VERSION_RCT 1,0,0,6
+#define _VERSION "1.0.0.7"
+#define _VERSION_RCS "1.0.0.7\0"
+#define _VERSION_RCT 1,0,0,7
